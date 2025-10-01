9c057b73ed2436e08f127af4f78faeffcaced909
ArielM24 <ariel_montoya15@outlook.es>
2025-10-01 12:45:00 -0600

    trades

--- src/main/java/com/sg/mods/SGMods.java
+++ src/main/java/com/sg/mods/SGMods.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.item.v1.DefaultItemComponentEvents;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -40,6 +41,9 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.Direction;
+import net.minecraft.village.TradeOffers.BuyItemFactory;
+import net.minecraft.village.TradeOffers.SellItemFactory;
+import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.Difficulty;
 import net.minecraft.state.property.Properties;
 
@@ -259,6 +263,7 @@ public class SGMods implements ModInitializer {
 		initRotation();
 		initStacks();
 		initTotem();
+		initTrades();
 	}
 
 	public static void initBedrock(){
@@ -534,6 +539,85 @@ public class SGMods implements ModInitializer {
 			player.getEntityWorld().sendEntityStatus(player, (byte)35);
 			totemstack.setCount(totemstack.getCount() - 1);
 			return false;
+		});
+	}
+
+	public static void initTrades(){
+		// ARMORER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.ARMORER, 3, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.DIAMOND_HORSE_ARMOR, 20, 1, 4, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.ARMORER, 2, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.IRON_HORSE_ARMOR, 10, 1, 4, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.ARMORER, 2, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.GOLDEN_HORSE_ARMOR, 10, 1, 4, 1));
+		});
+		// BUTCHER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 4, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.EGG, 16, 12, 1, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 4, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.BLUE_EGG, 16, 12, 1, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.BUTCHER, 4, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.BROWN_EGG, 16, 12, 1, 1));
+		});
+		// CARTOGRAPHER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CARTOGRAPHER, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.BRUSH, 16, 1, 4, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CARTOGRAPHER, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.GLOW_ITEM_FRAME, 5, 1, 12, 1));
+		});
+		// CLERIC
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.CLERIC, 4, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.AMETHYST_SHARD, 1, 4, 16, 1));
+		});
+		// FARMER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.GLOW_BERRIES, 16, 4, 1, 1));
+		});
+		// FISHERMAN
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FISHERMAN, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.TROPICAL_FISH_BUCKET, 16, 1, 4, 1));
+		});
+		// FLETCHER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 1, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.ARROW, 16, 4, 4, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FLETCHER, 3, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.SPECTRAL_ARROW, 16, 4, 1, 10));
+		});
+		// LEATHERWORKER
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.LEATHERWORKER, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.BUNDLE, 16, 1, 4, 1));
+		});
+		// LIBRARIAN
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.WRITABLE_BOOK, 16, 1, 4, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 5, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.GLOW_INK_SAC, 5, 12, 1, 1));
+		});
+		// MASON
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 2, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.CLAY, 1, 2, 16, 1));
+		});
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.MASON, 1, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.RESIN_BRICK, 1, 16, 1, 1));
+		});
+		// SHEPHERD
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.SHEPHERD, 5, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.STRING, 1, 4, 8, 1));
+		});
+		// TOOLSMITH
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.TOOLSMITH, 1, (consumer) -> {
+			consumer.add(new BuyItemFactory(Items.COPPER_INGOT, 1, 12, 1, 1));
+		});
+		// WEAPONSMITH
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.WEAPONSMITH, 3, (consumer) -> {
+			consumer.add(new SellItemFactory(Items.TNT, 1, 4, 4, 1));
 		});
 	}
 }
