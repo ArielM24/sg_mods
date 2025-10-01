6d1866c4ad9d66feb2f2e4fc8d3cdc6d154fa507
ArielM24 <ariel_montoya15@outlook.es>
2025-10-01 12:49:42 -0600

    vaults

--- src/main/java/com/sg/mods/SGMods.java
+++ src/main/java/com/sg/mods/SGMods.java
@@ -13,7 +13,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FacingBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.VaultBlockEntity;
 import net.minecraft.block.enums.Orientation;
+import net.minecraft.block.vault.VaultConfig;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
@@ -34,6 +37,7 @@ import net.minecraft.item.Items;
 import net.minecraft.item.equipment.trim.ArmorTrim;
 import net.minecraft.item.equipment.trim.ArmorTrimMaterial;
 import net.minecraft.item.equipment.trim.ArmorTrimPattern;
+import net.minecraft.loot.LootTable;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -52,6 +56,7 @@ import net.minecraft.state.property.Properties;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -268,6 +273,7 @@ public class SGMods implements ModInitializer {
 		initTotem();
 		initTrades();
 		initUnlimitedTrades();
+		initVaults();
 	}
 
 	public static void initBedrock(){
@@ -639,6 +645,50 @@ public class SGMods implements ModInitializer {
 				offer.updateDemandBonus();
 			}
 			
+			return ActionResult.PASS;
+		});
+	}
+
+	public static void initVaults(){
+		UseBlockCallback.EVENT.register((player, world, hand, result) -> {
+			ItemStack itemInHand = player.getStackInHand(hand);
+			String interactingBlockName = world.getBlockState(result.getBlockPos()).getBlock().getName().getString();
+			boolean isUsingtrialKey = itemInHand.getItem().getName().getString()
+					.equals(Items.TRIAL_KEY.getName().getString());
+			boolean isInteractingWithVault = interactingBlockName.equals(Blocks.VAULT.getName().getString());
+			BlockEntity block = world.getBlockEntity(result.getBlockPos());
+			if (!(block instanceof VaultBlockEntity)) {
+				return ActionResult.PASS;
+			}
+			VaultBlockEntity vaultBlock = (VaultBlockEntity) block;
+			if (!isInteractingWithVault) {
+				return ActionResult.PASS;
+			}
+			if (!isUsingtrialKey) {
+				return ActionResult.PASS;
+			}
+			if (!vaultBlock.getCachedState().toString().contains("vault_state=inactive")) {
+				return ActionResult.PASS;
+			}
+			// getting block data
+			RegistryKey<LootTable> lootTable = vaultBlock.getConfig().lootTable();
+			double activationRange = vaultBlock.getConfig().activationRange();
+			double deactivationRange = vaultBlock.getConfig().deactivationRange();
+			ItemStack keyItem = vaultBlock.getConfig().keyItem();
+			Direction facing = vaultBlock.getCachedState().get(Properties.HORIZONTAL_FACING);
+			boolean isOminous = vaultBlock.getCachedState().get(Properties.OMINOUS);
+
+			// creating new block
+			world.breakBlock(result.getBlockPos(), false);
+			BlockState s = Blocks.VAULT.getDefaultState().with(Properties.OMINOUS, isOminous)
+					.with(Properties.HORIZONTAL_FACING, facing);
+			world.setBlockState(result.getBlockPos(), s);
+			player.increaseStat(Stats.USED.getOrCreateStat(keyItem.getItem()), 1);
+			itemInHand.setCount(itemInHand.getCount() - 1);
+			VaultBlockEntity newVault = (VaultBlockEntity) world.getBlockEntity(result.getBlockPos());
+			Optional<RegistryKey<LootTable>> optionalLootTable = Optional.of(lootTable);
+			newVault.setConfig(
+					new VaultConfig(lootTable, activationRange, deactivationRange, keyItem, optionalLootTable));
 			return ActionResult.PASS;
 		});
 	}
