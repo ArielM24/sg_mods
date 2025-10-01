b4414a874378da3182af52c8eb8fe1a7166c4a9b
ArielM24 <ariel_montoya15@outlook.es>
2025-10-01 11:44:37 -0600

    bedrock

--- src/main/java/com/sg/mods/SGMods.java
+++ src/main/java/com/sg/mods/SGMods.java
@@ -1,6 +1,12 @@
 package com.sg.mods;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +21,25 @@ public class SGMods implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		initBedrock();
+	}
 
-		LOGGER.info("Hello Fabric world!");
+	public static void initBedrock(){
+		UseBlockCallback.EVENT.register((player, world, hand, result) -> {
+			ItemStack itemInHand = player.getStackInHand(hand);
+			String interactingBlockName = world.getBlockState(result.getBlockPos()).getBlock().getName().getString();
+			boolean isUsingDragonBreath = itemInHand.getItem().getName().getString()
+					.equals(Items.DRAGON_BREATH.getName().getString());
+			boolean isInteractingWithBedrock = interactingBlockName.equals(Blocks.BEDROCK.getName().getString());
+			if(!isInteractingWithBedrock || !isUsingDragonBreath){
+			return ActionResult.PASS;
+				
+			}
+			world.breakBlock(result.getBlockPos(), false);
+			player.increaseStat(Stats.USED.getOrCreateStat(itemInHand.getItem()), 1);
+			itemInHand.setCount(itemInHand.getCount() - 1);
+			player.giveItemStack(new ItemStack(Items.GLASS_BOTTLE));
+			return ActionResult.PASS;
+		});
 	}
 }
