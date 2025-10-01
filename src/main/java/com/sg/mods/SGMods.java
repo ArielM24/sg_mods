f1f17afd75eeb120757496f30cf7edab9b338017
ArielM24 <ariel_montoya15@outlook.es>
2025-10-01 12:54:08 -0600

    wandering trades

--- src/main/java/com/sg/mods/SGMods.java
+++ src/main/java/com/sg/mods/SGMods.java
@@ -18,6 +18,7 @@ import net.minecraft.block.entity.VaultBlockEntity;
 import net.minecraft.block.enums.Orientation;
 import net.minecraft.block.vault.VaultConfig;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
@@ -38,6 +39,7 @@ import net.minecraft.item.equipment.trim.ArmorTrim;
 import net.minecraft.item.equipment.trim.ArmorTrimMaterial;
 import net.minecraft.item.equipment.trim.ArmorTrimPattern;
 import net.minecraft.loot.LootTable;
+import net.minecraft.potion.Potions;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -46,6 +48,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOffers.BuyItemFactory;
@@ -53,6 +56,7 @@ import net.minecraft.village.TradeOffers.SellItemFactory;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.Difficulty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.village.TradeOffers.Factory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -274,6 +278,7 @@ public class SGMods implements ModInitializer {
 		initTrades();
 		initUnlimitedTrades();
 		initVaults();
+		initWanderingTrades();
 	}
 
 	public static void initBedrock(){
@@ -690,6 +695,93 @@ public class SGMods implements ModInitializer {
 			newVault.setConfig(
 					new VaultConfig(lootTable, activationRange, deactivationRange, keyItem, optionalLootTable));
 			return ActionResult.PASS;
+		});
+	}
+
+	public static void initWanderingTrades(){
+		TradeOfferHelper.registerWanderingTraderOffers((consumer)-> {
+			ArrayList<Factory> trades = new ArrayList<>();
+			// pottery sherds
+			trades.add(new SellItemFactory(Items.ANGLER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.ARCHER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.ARMS_UP_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.BLADE_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.BREWER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.BURN_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.DANGER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.FLOW_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.EXPLORER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.FRIEND_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.GUSTER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.HEART_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.HEARTBREAK_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.HOWL_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.MINER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.MOURNER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.PLENTY_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.PRIZE_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.SCRAPE_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.SHEAF_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.SHELTER_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.SKULL_POTTERY_SHERD, 2, 16, 4, 1));
+			trades.add(new SellItemFactory(Items.SNORT_POTTERY_SHERD, 2, 16, 4, 1));
+			// banner patterns
+			trades.add(new SellItemFactory(Items.FIELD_MASONED_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.BORDURE_INDENTED_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.FLOWER_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.CREEPER_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.SKULL_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.MOJANG_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.GLOBE_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.PIGLIN_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.FLOW_BANNER_PATTERN, 1, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.GUSTER_BANNER_PATTERN, 1, 1, 1, 1));
+			// logs
+			trades.add(new SellItemFactory(Items.BAMBOO_BLOCK, 1, 8, 8, 1));
+			trades.add(new SellItemFactory(Items.CRIMSON_STEM, 1, 8, 8, 1));
+			trades.add(new SellItemFactory(Items.WARPED_STEM, 1, 8, 8, 1));
+			// special
+			trades.add(new SellItemFactory(Items.HEART_OF_THE_SEA, 32, 1, 2, 1));
+			trades.add(new SellItemFactory(Items.SHULKER_SHELL, 32, 1, 2, 1));
+			trades.add(new SellItemFactory(Items.OMINOUS_TRIAL_KEY, 32, 1, 2, 1));
+			trades.add(new SellItemFactory(Items.TRIAL_KEY, 8, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.ECHO_SHARD, 1, 1, 8, 1));
+			trades.add(new SellItemFactory(Items.HEAVY_CORE, 64, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.WITHER_SKELETON_SKULL, 64, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.WITHER_ROSE, 2, 1, 16, 1));
+			trades.add(new SellItemFactory(Items.DRAGON_BREATH, 4, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.ENCHANTED_GOLDEN_APPLE, 64, 1, 1, 1));
+			trades.add(new SellItemFactory(Items.SNIFFER_EGG, 8, 1, 2, 1));
+			trades.add(new SellItemFactory(Items.BREEZE_ROD, 16, 4, 4, 1));
+			trades.add(new SellItemFactory(Items.NETHERITE_SCRAP, 16, 4, 4, 1));
+			trades.add(new SellItemFactory(Items.TNT, 16, 4, 4, 1));
+			trades.add(new SellItemFactory(Items.END_CRYSTAL, 16, 4, 4, 1));
+			trades.add(new SellItemFactory(Items.SPONGE, 16, 4, 4, 1));
+			trades.add(new SellItemFactory(Items.HONEY_BLOCK, 16, 16, 4, 1));
+			
+			// potions
+			trades.add(new SellItemFactory(PotionContentsComponent.createStack(Items.POTION, Potions.LUCK), 8, 1, 4, 1));
+			// trim templates
+			trades.add(new SellItemFactory(Items.SENTRY_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.VEX_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.WILD_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.COAST_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.DUNE_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.RAISER_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.HOST_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.WARD_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE, 64, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.TIDE_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.SNOUT_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.RIB_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.EYE_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.SPIRE_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.FLOW_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.BOLT_ARMOR_TRIM_SMITHING_TEMPLATE, 32, 1, 4, 1));
+			trades.add(new SellItemFactory(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE, 64, 1, 4, 1));
+			consumer.pool(Identifier.of(MOD_ID), 1, trades);
 		});
 	}
 }
